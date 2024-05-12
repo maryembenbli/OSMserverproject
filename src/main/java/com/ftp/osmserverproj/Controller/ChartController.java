059c82ab75851456c3f70ceeee2d3aaefcc9b7c2
unknown <meryembenbli4@gmail.com>
2024-05-12 11:35:29 +0200

    Fix ChartController.java content

--- src/main/java/com/ftp/osmserverproj/Controller/ChartController.java
+++ src/main/java/com/ftp/osmserverproj/Controller/ChartController.java
@@ -1,2 +1,35 @@
-package com.ftp.osmserverproj.Controller;public class ChartController {
+package com.ftp.osmserverproj.Controller;
+
+import com.ftp.osmserverproj.Service.ChartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/charts")
+public class ChartController {
+    @Autowired
+    private ChartService chartService;
+
+    @GetMapping("/sales-by-date")
+    public ResponseEntity<Map<String, Integer>> getSalesByDate() {
+        Map<String, Integer> salesByDate = chartService.getSalesByDate();
+        return ResponseEntity.ok(salesByDate);
+    }
+
+    @GetMapping("/sales-by-place")
+    public ResponseEntity<Map<String, Integer>> getSalesByPlace() {
+        Map<String, Integer> salesByPlace = chartService.getSalesByPlace();
+        return ResponseEntity.ok(salesByPlace);
+    }
+
+    @GetMapping("/gender-distribution")
+    public ResponseEntity<Map<String, Integer>> getGenderDistribution() {
+        Map<String, Integer> genderDistribution = chartService.getGenderDistribution();
+        return ResponseEntity.ok(genderDistribution);
+    }
 }
